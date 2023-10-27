593436ba2bef8c4c351b6da2331abba72516ce2e
varun agarwal <varunagarwal1710@gmail.com>
2023-10-27 20:47:29 +0530

    Feat added PDF Render

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -34,7 +34,7 @@ const Page = async ({ params }: PageProps) => {
 				{/* Left panel */}
 				<div className="flex-1 xl:flex">
 					<div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
-						<PDFRenderer />
+						<PDFRenderer url={file.url} />
 					</div>
 				</div>
 
